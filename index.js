9eca19ac9c1a61c5b7bc3937ebaa9468dab6db24
Fallfromgrace18 <149462981+Fallfromgrace18@users.noreply.github.com>
2024-03-04 18:01:47 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const ul=document.createElement("ul")
+main.remove("main")
+ const newHeader=document.createElement('h1')
+ newHeader.id="victory"
+ newHeader.textContent="Catherine Morelli is the champion"
+document.body.append(newHeader)
